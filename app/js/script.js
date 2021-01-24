c1cd4d0954ded7d27a04d97cbc43e02459818ac8
Ted <tchbell@gmail.com>
2021-01-23 19:58:46 -0500

    added header

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,24 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
-header.addEventListener('click', () => {
-    console.log('click');
+const fadeElems = document.querySelectorAll('.has-fade');
 
+header.addEventListener('click', () => {
     if (header.classList.contains('open')) {
         header.classList.remove('open');
-        overlay.classList.remove('fade-in')
-        overlay.classList.add('fade-out');
+        body.classList.remove('noscroll');
+        fadeElems.forEach(element => {
+            element.classList.add('fade-out');
+            element.classList.remove('fade-in');
+        })
     } else {
         header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        body.classList.add('noscroll');
+        fadeElems.forEach(element => {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        })
     }
 
 })
